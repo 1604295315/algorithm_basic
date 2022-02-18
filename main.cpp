d325e69d143815d481f3ec7f16c9aac4f5b87f01
zhang <zhang@DESKTOP-I9AOEMQ>
2022-02-18 17:25:34 +0800

    initial project

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, const char** argv) {
+
+    cout << "Algorithm_Basic_Study_Record !" << endl;
+    return 0;
+}
