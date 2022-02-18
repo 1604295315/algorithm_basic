6c67ea3f8febd7dda35b16f7bbf8d9179e0191df
zhang <zhang@DESKTOP-I9AOEMQ>
2022-02-18 21:36:31 +0800

    Merge branch 'master' of https://github.com/1604295315/algorithm_basic

---
+++ src/chapter01/1012.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+const int N = 1e6 + 10;
+
+void merge_sort()
+{
+
+}
+int n;
+int q[N];
+
+int main(int argc, const char** argv) {
+    scanf("%d", &n);
+    return 0;
+}
