fd917822f08629db376c6fc10d72394294b3cdc6
elinjhon <1604295315@qq.com>
2022-03-19 17:06:26 +0800

    t786

--- src/03week1/786.cpp
+++ src/03week1/786.cpp
@@ -1,25 +1,9 @@
-#include <iostream>
+#include <stdio.h>
 
-int Partition(int q[], int l, int r) {
-    int pivot = q[l];
-    while (l < r)
-    {
-        while( l < r && q[r] >= pivot) -- r;
-        q[l] = q[r];
-        while( l < r && q[l] <= pivot) ++ l;
-        q[r] = q[l];
-    }
-    q[l] = pivot;
-    return l;
-}
+const int N = 1e6 + 10;
 
-void QuickSort(int q[], int l, int r) {
-    if (l < r) {
-        int pivotpos = Partition(q, l, r);
-        QuickSort(q, l, pivotpos - 1);
-        QuickSort(q, pivotpos + 1, r);
-    }
-}
+int n;
+int q[N];
 
 void quick_sort(int q[], int l, int r)
 {
@@ -42,3 +26,51 @@ void quick_sort(int q[], int l, int r)
     quick_sort(q,l,j);
     quick_sort(q, j+1, r); 
 }
+
+int main(int argc, char const *argv[])
+{
+    scanf("%d", &n);
+    for (int i = 0; i < n; i ++)
+        scanf("%d", &q[i]);
+    
+    quick_sort(q, 0, n-1);
+
+    for (int i = 0; i < n; i ++)
+    {
+        i && printf(" ");
+        printf("%d", q[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+// int main(int argc, const char** argv) {
+//     if(argc <= 1)
+//         printf("0 argc\n");
+//     else
+//         printf("%d args\n", argc - 1);
+//     return 0;
+// }
+/*
+int Partition(int q[], int l, int r) {
+    int pivot = q[l];
+    while (l < r)
+    {
+        while( l < r && q[r] >= pivot) -- r;
+        q[l] = q[r];
+        while( l < r && q[l] <= pivot) ++ l;
+        q[r] = q[l];
+    }
+    q[l] = pivot;
+    return l;
+}
+
+void QuickSort(int q[], int l, int r) {
+    if (l < r) {
+        int pivotpos = Partition(q, l, r);
+        QuickSort(q, l, pivotpos - 1);
+        QuickSort(q, pivotpos + 1, r);
+    }
+}
+
+*/
