76c8d993544c7e0a7f5abe80a7c46f5a7788768f
ELinJhon <1604295315@qq.com>
2022-04-13 19:39:20 +0800

    0201 LinkList Stack Queue

--- src/05chapter02/D0203.cpp
+++ src/05chapter02/D0203.cpp
@@ -3,24 +3,50 @@
 // 滑动窗口里的最大值与最小值
 // 单调队列
 
-// n k 1e6
-// 单调队列 最小值为队头?
+// n k 1e6 ^2 O(nk)
+// 单调队列 最小值为队头
 #include <stdio.h>
 
 const int N = 1000010;
+// 1e6;
 
-int q[N], tt, hh;
-
-int n;
+int a[N], q[N];
+int n,k;
 
 int main() {
-    scanf("%d", &n);
+    scanf("%d%d", &n, &k);
     for (int i = 0; i < n; i ++)
-        scanf("%d", &q[i]);
-        
-    int hh = 0; tt = -1;
+        scanf("%d", &a[i]);
+// 双指针和滑动窗口
+// 单调队列
+// 暴力 挖掘 性质
+    int hh = 0, tt = -1;
     for (int i = 0; i < n; i ++) {
-        
+        // 判断队头是否已经滑出窗口
+        // 空           i-k+1 终点 qhh 该出去了
+        if (hh <= tt && i - k + 1 > q[hh])
+            hh ++;
+        // 新数字比队尾小， 对位无用了， 队尾删掉
+        while( hh <= tt && a[q[tt]] >= a[i])
+            tt --;
+        q[++ tt] = i;
+        if (i >= k - 1)
+            printf("%d ", a[q[hh]]);
     }
+    puts("");
+    hh = 0, tt = -1;
+    for (int i = 0; i < n; i ++) {
+        // 判断队头是否已经滑出窗口
+        // 非空           i-k+1 终点 qhh 该出去了
+        if (hh <= tt && i - k + 1 > q[hh])
+            hh ++;
+        // 新数字比队尾小， 对位无用了， 队尾删掉
+        while( hh <= tt && a[q[tt]] <= a[i])
+            tt --;
+        q[++ tt] = i;
+        if (i >= k - 1)
+            printf("%d ", a[q[hh]]);
+    }
+    puts("");
     return 0;
 }
