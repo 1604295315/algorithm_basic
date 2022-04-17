ed842a0e064041d7a08cf61e3cb6f5d84f3febfa
ELinJhon <1604295315@qq.com>
2022-04-17 20:49:17 +0800

    chapter02 数据结构 trie 并查集 堆

---
+++ src/07chapter02/D0601.cpp
@@ -0,0 +1,39 @@
+// 堆
+// down
+// up
+// AcWing 838. 堆排序
+#include <iostream>
+
+using namespace std;
+
+const int N = 100010;
+
+int n, m;
+int h[N], s;
+
+int down(int u) {
+    int t = u;
+    if (u * 2 <= s && h[u * 2] < h[t]) t = u * 2;
+    if (u * 2 + 1 <= s && h[u * 2 + 1] < h[t]) t = u * 2 + 1;
+    if (u != t) {
+        swap(h[u], h[t]);
+        down(t);
+    }
+}
+
+int main() {
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i ++) {
+        scanf("%d", &h[i]);
+    }
+
+    s = n;
+    for (int i = n/2; i; i --) down(i);
+
+    while (m --) {
+        printf("%d ", h[1]);
+        h[1] = h[s --];
+        down(1);
+    }
+    return 0;
+}
